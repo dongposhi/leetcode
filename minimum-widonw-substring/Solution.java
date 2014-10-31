a8f2cddc8a1faee9cb2580bbd0d5545c55ddf95e
dongposhi <shikou1@gmail.com>
2014-10-31 16:57:14 +0800

    minimum-window-substring

--- minimum-widonw-substring/Solution.java
+++ minimum-widonw-substring/Solution.java
@@ -1,6 +1,15 @@
 public class Solution{
 
     public String minWindow(String s, String t){
-         return "";  
+
+    	String ret = "";
+
+    	List<Integer> matchs = new ArrayList<Integer>();
+    	for(int i = 0;i < s.length();i++){
+    		if(t.indexOf(s.getBytes()[i]) >=0){
+
+    		}
+    	}
+        return ret;  
     }
 }
