9953fdee98cdeb0005830775eddf1ad77e3168a2
dongposhi <shikou1@gmail.com>
2014-11-03 14:58:08 +0800

    init palindromeNumber

---
+++ palindromeNumber/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+    	boolean ret = true;
+
+    	System.out.println(Math.pow(10,4));	
+    	return ret;
+    }
+
+    public static void main(String[] args){
+    	int x = Integer.parseInt(args[0]);
+    	Solution s = new Solution();
+
+    	System.out.println(s.isPalindrome(x));
+    }
+}
