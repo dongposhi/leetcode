bf093a1cb32059962832c5a067de71d153872318
dongposhi <shikou1@gmail.com>
2014-11-05 08:39:28 +0800

    Finish palindromeNumber

--- palindromeNumber/Solution.java
+++ palindromeNumber/Solution.java
@@ -1,12 +1,22 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-    	boolean ret = true;
+        if (x < 0){
+            return false;
+        }
+        long m = 1L *x ;
+        long y = 0;
+        while ( m > 0){
+            y = y*10 + m%10;
+            m = m/10;
+        }
 
-    	System.out.println(Math.pow(10,4));	
-    	return ret;
+        return x == y;
     }
 
     public static void main(String[] args){
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
     	int x = Integer.parseInt(args[0]);
     	Solution s = new Solution();
 
